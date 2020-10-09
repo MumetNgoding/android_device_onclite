3ce5f49891b058e7bf3378d454630eca3df8d439
MumetNgoding <nugroho31.com@gmail.com>
2020-10-09 20:45:27 +0700

    Onclite: Update Fingerprint from Coral October Patches

--- superior_onclite.mk
+++ superior_onclite.mk
@@ -28,10 +28,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys" 
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RP1A.201005.004 6782484 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 

f5092a5e2fdd0d3450672eafbd4e82e9110ed685
MumetNgoding <bryanhafidz@hack.id>
2020-11-25 20:26:30 +0000

    Onclite: Bringup to Superior

--- superior_onclite.mk
+++ superior_onclite.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some Pixys stuff.
-$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+# Inherit some common Superior stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Bootanimation res
 TARGET_BOOT_ANIMATION_RES := 720
@@ -22,7 +22,7 @@ TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := pixys_onclite
+PRODUCT_NAME := superior_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
